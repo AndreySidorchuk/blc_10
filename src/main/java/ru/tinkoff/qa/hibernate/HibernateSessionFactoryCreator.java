1234225d35981578196dfb3ade9760e793ff4f2f
AndreySidorchuk <67549724+AndreySidorchuk@users.noreply.github.com>
2023-12-14 02:05:20 +0400

    Update HibernateSessionFactoryCreator.java

--- src/main/java/ru/tinkoff/qa/hibernate/HibernateSessionFactoryCreator.java
+++ src/main/java/ru/tinkoff/qa/hibernate/HibernateSessionFactoryCreator.java
@@ -1,9 +1,25 @@
 package ru.tinkoff.qa.hibernate;
 
+
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateSessionFactoryCreator {
-    public static SessionFactory createSessionFactory(){
-        return null;
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory createSessionFactory() {
+        try {
+            sessionFactory = new Configuration()
+                    .addAnnotatedClass(Workman.class)
+                    .addAnnotatedClass(Animal.class)
+                    .addAnnotatedClass(Places.class)
+                    .addAnnotatedClass(Zoo.class)
+                    .configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        return sessionFactory;
     }
 }
