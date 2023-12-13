97730bc0cb9a8a6e0c1d3924f6e858f6ce5b95ea
AndreySidorchuk <67549724+AndreySidorchuk@users.noreply.github.com>
2023-12-14 02:03:50 +0400

    Update ZooHibernateTests.java

--- src/test/java/ru/tinkoff/qa/hibernate/ZooHibernateTests.java
+++ src/test/java/ru/tinkoff/qa/hibernate/ZooHibernateTests.java
@@ -1,14 +1,33 @@
 package ru.tinkoff.qa.hibernate;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import jakarta.persistence.PersistenceException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.*;
 import ru.tinkoff.qa.BeforeCreator;
 
+import java.util.List;
+
 public class ZooHibernateTests {
+
+    private static SessionFactory sessionFactory = null;
+    private Session session = null;
 
     @BeforeAll
     static void init() {
         BeforeCreator.createData();
+    }
+
+    @BeforeEach
+    void setupThis() {
+        sessionFactory = HibernateSessionFactoryCreator.createSessionFactory();
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+    }
+
+    @AfterAll
+    static void tear() {
+        sessionFactory.close();
     }
 
     /**
@@ -16,7 +35,8 @@ public class ZooHibernateTests {
      */
     @Test
     public void countRowAnimal() {
-        assert false;
+        List<Integer> list = session.createQuery("SELECT a.id FROM Animal a", Integer.class).getResultList();
+        Assertions.assertEquals(10, list.size());
     }
 
     /**
@@ -24,7 +44,25 @@ public class ZooHibernateTests {
      */
     @Test
     public void insertIndexAnimal() {
-        assert false;
+        for (int i = 1; i <= 10; i++) {
+            Animal animal = new Animal();
+            animal.setId(i);
+            animal.setName("Соня");
+            animal.setAge(3);
+            animal.setType(2);
+            animal.setSex(2);
+            animal.setPlace(2);
+            session.save(animal);
+            try {
+                session.getTransaction().commit();
+            } catch (IllegalStateException | PersistenceException e) {
+                if (e.getClass() != PersistenceException.class) {
+                    Assertions.assertTrue(e.getMessage().contains("Transaction not successfully started"));
+                } else {
+                    Assertions.assertTrue(e.getMessage().contains("Converting `org.hibernate.exception.ConstraintViolationException` to JPA"));
+                }
+            }
+        }
     }
 
     /**
@@ -32,7 +70,16 @@ public class ZooHibernateTests {
      */
     @Test
     public void insertNullToWorkman() {
-        assert false;
+        Workman workman = new Workman();
+        workman.setId(11);
+        workman.setName(null);
+        workman.setAge(2);
+        workman.setPosition(2);
+        session.save(workman);
+        PersistenceException exception = Assertions.assertThrows(PersistenceException.class, () -> {
+            session.getTransaction().commit();
+        });
+        Assertions.assertTrue(exception.getMessage().contains("Converting `org.hibernate.exception.ConstraintViolationException` to JPA"));
     }
 
     /**
@@ -40,7 +87,15 @@ public class ZooHibernateTests {
      */
     @Test
     public void insertPlacesCountRow() {
-        assert false;
+        Places places = new Places();
+        places.setId(6);
+        places.setRow(2);
+        places.setPlaceNum(2);
+        places.setName("Орехово");
+        session.save(places);
+        session.getTransaction().commit();
+        List<Integer> listPlaces = session.createQuery("SELECT p.id FROM Places p", Integer.class).getResultList();
+        Assertions.assertEquals(6, listPlaces.size());
     }
 
     /**
@@ -48,6 +103,8 @@ public class ZooHibernateTests {
      */
     @Test
     public void countRowZoo() {
-        assert false;
+        List<String> names = session.createQuery("SELECT z.name FROM Zoo z", String.class).getResultList();
+        Assertions.assertEquals(3, names.size());
+        Assertions.assertEquals(names, List.of("Центральный", "Северный", "Западный"));
     }
 }
